7901f0f232491deca97ceeb46205a9aa1f6f4756
Aldin Ahmeti <88678065+AldinAhmeti@users.noreply.github.com>
2025-10-24 18:56:03 +0200

    Add files via upload

---
+++ Aldin.jsx
@@ -0,0 +1,34 @@
+export default function ArrowDown() {
+  return <div className="arrow"></div>;
+}
+import React from "react";
+import "./ArrowDown.css"; // we'll make this file next
+
+export default function ArrowDown({ onClick }) {
+  return (
+    <div 
+      className="arrow" 
+      onClick={onClick} 
+      title="Scroll down 👇"
+    ></div>
+  );
+}
+function ProjectCard() {
+  return (
+    <div className="project-card">
+      <div className="project-overlay">
+        <p>View Project</p>
+      </div>
+    </div>
+  );
+}
+<a
+  href="https://www.figma.com/proto/MYL8kyJz7hyJdYVnowQsBK/Postify?node-id=266-809"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="project-card"
+>
+  <div className="project-overlay">
+    <span>View Project</span>
+  </div>
+</a>
